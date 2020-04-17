16ea42ebf442d36c4089a39e6c6d7e4ff69ffe0c
kAGOTSI <cyrilleagotsi@gmail.com>
2020-04-17 13:31:53 +0200

    fix new bug.

--- src/test/java/gl51/tp1/MovieControllerTest.groovy
+++ src/test/java/gl51/tp1/MovieControllerTest.groovy
@@ -4,7 +4,10 @@ import gl51.tp1.movie.data.Movie
 import gl51.tp1.movie.data.MovieRequest
 import gl51.tp1.movie.service.MovieClient
 import gl51.tp1.movie.service.impl.MovieClientImpl
+import io.micronaut.core.type.Argument
+import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.client.RxHttpClient
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.runtime.server.EmbeddedServer
@@ -17,39 +20,40 @@ import spock.lang.Specification
 
 import javax.inject.Inject
 
-    @MicronautTest
-    class MovieControllerSpec extends Specification {
-        @Shared @Inject
-        EmbeddedServer embeddedServer
+@MicronautTest
+class MovieControllerSpec extends Specification {
 
-        @Shared @AutoCleanup @Inject @Client("/")
-        RxHttpClient client
+    @Shared @Inject
+    EmbeddedServer embeddedServer
 
-        void "testIndex"() {
-            given:
-            Flowable flowable = client.retrieve(HttpRequest.GET("/movie"), Argument.listOf(Movie))
-            def content = flowable.firstElement().blockingGet()
-            expect:
-            content == []
-        }
+    @Shared @AutoCleanup @Inject @Client("/")
+    RxHttpClient client
 
-        void "testFilmCreation"() {
-            when:
-            HttpResponse response = client.toBlocking().exchange(
-                    HttpRequest.POST("/movie", new MovieRequest(imdbId: "aaaaa"))
-            )
-            Flowable flowable = client.retrieve(HttpRequest.GET("/movie"), Argument.listOf(Movie))
-            def content = flowable.firstElement().blockingGet()
-            then:
-            response.status == HttpStatus.CREATED
-            content.find { it.title == 'my movie' && it.imdbID == "aaaaa" }
-        }
+    void "testIndex"() {
+        given:
+        Flowable flowable = client.retrieve(HttpRequest.GET("/movie"), Argument.listOf(Movie))
+        def content = flowable.firstElement().blockingGet()
+        expect:
+        content == []
+    }
+
+    void "testFilmCreation"() {
+        when:
+        HttpResponse response = client.toBlocking().exchange(
+                HttpRequest.POST("/movie", new MovieRequest(imdbId: "aaaaa"))
+        )
+        Flowable flowable = client.retrieve(HttpRequest.GET("/movie"), Argument.listOf(Movie))
+        def content = flowable.firstElement().blockingGet()
+        then:
+        response.status == HttpStatus.CREATED
+        content.find { it.title == 'my movie' && it.imdbID == "aaaaa" }
+    }
 
 
-        @MockBean(MovieClientImpl)
-        MovieClient movieClient() {
-            def mock = Mock(MovieClient)
-            mock.getMovieDetail("aaaaa") >> new Movie(imdbID: "aaaaa", title: 'my movie')
-            mock
-        }
+    @MockBean(MovieClientImpl)
+    MovieClient movieClient() {
+        def mock = Mock(MovieClient)
+        mock.getMovieDetail("aaaaa") >> new Movie(imdbID: "aaaaa", title: 'my movie')
+        mock
+    }
 }
