92eb8cee73bc8368b3fb2032a2812cd5d16071e7
kAGOTSI <cyrilleagotsi@gmail.com>
2020-04-17 13:22:41 +0200

    Fix MovieControler bug.

--- src/test/java/gl51/tp1/MovieControllerTest.groovy
+++ src/test/java/gl51/tp1/MovieControllerTest.groovy
@@ -1,11 +1,15 @@
 package gl51.tp1
 
+import gl51.tp1.movie.data.Movie
 import gl51.tp1.movie.data.MovieRequest
+import gl51.tp1.movie.service.MovieClient
+import gl51.tp1.movie.service.impl.MovieClientImpl
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.client.RxHttpClient
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.runtime.server.EmbeddedServer
 import io.micronaut.test.annotation.MicronautTest
+import io.micronaut.test.annotation.MockBean
 import io.reactivex.Flowable
 import spock.lang.AutoCleanup
 import spock.lang.Shared
@@ -15,27 +19,37 @@ import javax.inject.Inject
 
     @MicronautTest
     class MovieControllerSpec extends Specification {
-
         @Shared @Inject
         EmbeddedServer embeddedServer
 
         @Shared @AutoCleanup @Inject @Client("/")
         RxHttpClient client
 
-        void "test index"() {
+        void "testIndex"() {
             given:
             Flowable flowable = client.retrieve(HttpRequest.GET("/movie"), Argument.listOf(Movie))
-            def content = flowable.firstElement()
+            def content = flowable.firstElement().blockingGet()
             expect:
-            content.blockingGet() == []
+            content == []
         }
 
-        void "test film creation"() {
-            given:
+        void "testFilmCreation"() {
+            when:
             HttpResponse response = client.toBlocking().exchange(
                     HttpRequest.POST("/movie", new MovieRequest(imdbId: "aaaaa"))
             )
-            expect:
+            Flowable flowable = client.retrieve(HttpRequest.GET("/movie"), Argument.listOf(Movie))
+            def content = flowable.firstElement().blockingGet()
+            then:
             response.status == HttpStatus.CREATED
+            content.find { it.title == 'my movie' && it.imdbID == "aaaaa" }
+        }
+
+
+        @MockBean(MovieClientImpl)
+        MovieClient movieClient() {
+            def mock = Mock(MovieClient)
+            mock.getMovieDetail("aaaaa") >> new Movie(imdbID: "aaaaa", title: 'my movie')
+            mock
         }
 }
